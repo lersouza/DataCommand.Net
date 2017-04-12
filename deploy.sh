235dc97c6e62f8649648f9f61b99ffb148b6a623
Leandro <le.rsouza@gmail.com>
2017-04-12 09:42:33 -0300

    Set main branch to master

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ "$TRAVIS_BRANCH" == "prod" ]; then
+if [ "$TRAVIS_BRANCH" == "master" ]; then
 	dotnet pack -c Release -o ./release
 	nuget push ./src/DataCommand.Core/release/*.nupkg $NUGET_API_KEY -Source https://www.nuget.org/api/v2/package
 fi
